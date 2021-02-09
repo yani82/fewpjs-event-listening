91ae34a6295c84da38bd653f499413049853cdc8
yani82 <yani.iman1@gmail.com>
2021-02-09 00:56:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+      }); 
 }
